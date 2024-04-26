07f4ebe52bb497044323eb9549a8af2de1a72a07
sandra <sandrasamir108@gmail.com>
2024-04-26 23:17:57 +0300

    creating login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+function LoginPage(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    };
+
+    return (
+        <div className="container">
+            <form className="Login_form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    placeholder="youremail@gmail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <label htmlFor="password"> Password </label>
+                <input
+                    type="password"
+                    placeholder="**********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="Submit"> Login </button>
+            </form>
+            <h5> Don't have an account?</h5>
+            <button className="link-button" onClick={() => props.OnFormSwitch('signup')}>
+                {' '}
+                Create an account 
+            </button>
+        </div>
+    );
+}
+
+export default LoginPage;
