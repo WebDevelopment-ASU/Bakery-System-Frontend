21a89db240f81f906436b55ade3982d047affb07
sandra <sandrasamir108@gmail.com>
2024-04-27 22:40:34 +0300

    Creating login and signup footer

---
+++ src/components/Credentials-Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Credentials-Footer.module.css';
+
+function Footer() {
+    return <footer className={styles.mainfooter}>© Bakery System 2024</footer>;
+}
+
+export default Footer;
