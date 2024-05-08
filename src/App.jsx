3894a27046d036f51da9a15af5528c96109e2a26
Youssef Soliman <youssef.soly03@gmail.com>
2024-05-08 19:09:39 +0300

    Add image logo and fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from './App.module.css';
-import Navbar from './Components/Navbar';
+import Navbar from './components/Navbar.jsx';
 import LandingPage from './pages/LandingPage/LandingPage.jsx';
 // import Landing from './Components/Landing'
 function App() {
