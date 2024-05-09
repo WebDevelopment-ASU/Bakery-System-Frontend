1edc7401bc35f3c5ae7bcbda5984e81ef8600be1
sandra <sandrasamir108@gmail.com>
2024-05-10 00:30:07 +0300

    some modifications in the profile page adding more buttons and modifying css file

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import httpClient from '../../utils/httpClient'; 
 import styles from './ProfilePage.module.css';
-
 const ProfilePage = () => {
   const [user, setUser] = useState({
     username: '',
@@ -44,15 +43,35 @@ const ProfilePage = () => {
     });
   };
 
+
+
+  // const validatePassword = async () => {
+  //   try {
+  //     const response = await httpClient.post('users/{id}/validate-password', {
+  //       password: user.password
+  //     });
+      
+  //     if (response.data.success) {
+  //       alert('Password is valid!');
+  //     } else {
+  //       alert('Password is invalid!');
+  //     }
+  //   } catch (error) {
+  //     console.error('Password validation failed:', error);
+  //     alert('Password validation failed!');
+  //   }
+  // };
+  
+
   return (
     <>
       <div className={styles.container}>
       <h2 className={styles.h2}>Profile Info</h2>
-      <div className={styles.photo}> <img src="background.jpg" alt="" /> </div>
+      {/* <div className={styles.photo}> <img src="background.jpg" alt="" /> </div> */}
       <form onSubmit={handleSubmit} className={styles.profile_form}>
         <div className={styles.profile}>
           <label className={styles.label}>Username :    </label>
-          <input className={styles.input}
+          <input className={styles.input1}
             type="text"
             name="username"
             value={user.username}
@@ -61,7 +80,7 @@ const ProfilePage = () => {
         </div>
         <div className={styles.profile}>
           <label className={styles.label}>Email :    </label>
-          <input className={styles.input}
+          <input className={styles.input2}
             type="email"
             name="email"
             value={user.email}
@@ -70,17 +89,27 @@ const ProfilePage = () => {
         </div>
         <div className={styles.profile}>
           <label className={styles.label}>Role :    </label>
-          <input className={styles.input}
+          <input className={styles.input3}
             type="text"
             name="role"
             value={user.role}
             onChange={handleChange}
           />
+          <div className={styles.profile}>
+          <input className={styles.input4}
+            type="text"
+            name="Password"
+            value={user.Password}
+            placeholder="Enter password"
+            onChange={handleChange}
+          />
+        <button type="submit"  className={styles.Password_button}>Validate Password</button>
+          </div>
         </div>
         <button type="submit"  className={styles.Edit_button}>Update Profile</button>
-        <button type="submit"  className={styles.Edit_button}>Delete Profile</button>
-
-      </form>
+        <button type="submit"  className={styles.change_button}>Change Password</button>
+        <button type="submit"  className={styles.delete_button}>Delete User</button>
+             </form>
     </div>
     </>
   );
