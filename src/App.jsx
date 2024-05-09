6365d8dcafb5f7f92563cbe1e93ddc9279dcdbfc
Mark Ramy <markramyy@gmail.com>
2024-05-09 11:00:45 +0300

    Restrict Navbar to the Landing Page only

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import styles from './App.module.css';
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/login/LoginPage';
 import SignUpPage from './pages/signup/SignupPage';
@@ -8,8 +8,7 @@ import SignUpPage from './pages/signup/SignupPage';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
+      <div className={styles.App}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
