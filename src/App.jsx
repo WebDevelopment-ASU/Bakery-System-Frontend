cfcb1439b0913c594f862fd272e752b86aacb3b1
Mark Ramy <markramyy@gmail.com>
2024-05-12 14:01:58 +0300

    refactor: Update import URL and remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import LoginPage from './pages/login/LoginPage';
-import SignupPage from './pages/signup/SignupPage';
-import ProfilePage from './pages/Profile/ProfilePage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/login" component={LoginPage} />
-                <Route path="/signup" component={SignupPage} />
                 <Route path="/profile" component={ProfilePage} />
-                <Route path="/" exact component={LoginPage} />
             </Switch>
         </Router>
     );
