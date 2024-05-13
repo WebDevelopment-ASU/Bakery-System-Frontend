8ec0288e37aae59d473d11e06969941948152515
Mark Ramy <markramyy@gmail.com>
2024-05-13 12:08:01 +0300

    fix image import

--- src/Components/credentials/Credentials-Header.jsx
+++ src/Components/credentials/Credentials-Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Credentials-Header.module.css';
+import BackeryLogo from '../../assets/backeryLogo.png';
 
 function Header() {
     return (
         <header className={styles.main}>
-            <img src="/Bakery-Logo.png" alt="logo" />
+            <img src={BackeryLogo} alt="logo" />
         </header>
     );
 }
